1899b32d50aee7d28944c4f2ad351459c62569e2
Kiran Kumbhar <kiran.kumbhar@protegrity.com>
2019-03-09 18:32:35 +0530

    Order now button added

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -11,15 +11,17 @@ const controls = [
 
 const buildControls = (props) => (
     <div className = {classes.BuildControls}>
-    {controls.map(control => (
-        <BuildControl 
-        key = {control.label} 
-        label = {control.label} 
-        added = {() => props.ingredientAdded(control.type)} 
-        removed = {() => props.ingredientRemoved(control.type)} 
-        disabled = {props.disabled[control.type]}
-         />
-    ))}
+        <p>Total Price <strong>${props.price.toFixed(2)}</strong></p>
+        {controls.map(control => (
+            <BuildControl 
+            key = {control.label} 
+            label = {control.label} 
+            added = {() => props.ingredientAdded(control.type)} 
+            removed = {() => props.ingredientRemoved(control.type)} 
+            disabled = {props.disabled[control.type]}
+            />
+        ))}
+        <button className = {classes.OrderButton} disabled = {props.purchasable}>ORDER NOW</button>
     </div>
 );
 
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -19,7 +19,23 @@ class BurgerBuilder extends Component {
             cheese: 0,
             meat: 0,
         },
-        totalPrice: 4
+        totalPrice: 4,
+        purchasable: false,
+    }
+
+    updatePurchaseState = () => {
+        const ingredients = {
+            ...this.state.ingredients
+        };
+        const sum = Object.keys(ingredients)
+        .map(iKey => {
+            return ingredients[iKey];
+        })
+        .reduce((sum, el) => {
+            return sum + el;
+        }, 0);
+
+        this.setState({purchasable: sum > 0});
     }
 
     addingredientHandler = (type) => {
@@ -64,6 +80,8 @@ class BurgerBuilder extends Component {
                     ingredientAdded={this.addingredientHandler}
                     ingredientRemoved = {this.removeIngredientHandler}
                     disabled = {disabledInfo}
+                    purchasable = {this.state.purchasable}
+                    price = {this.state.totalPrice}
                 />
             </Auxiliary>
         );
