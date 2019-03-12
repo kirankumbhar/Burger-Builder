f6b5bb496a7121e53f29d1134961bd0df1544e36
Kiran Kumbhar <kiran.kumbhar@protegrity.com>
2019-03-12 19:49:30 +0530

    Backdrop and modal implemented

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -24,6 +24,7 @@ class BurgerBuilder extends Component {
         },
         totalPrice: 4,
         purchasable: false,
+        purchasing: false
     }
 
     updatePurchaseState = (ingredients) => {
@@ -66,6 +67,15 @@ class BurgerBuilder extends Component {
         this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
         this.updatePurchaseState(updatedIngredients);
     }
+
+    purchaseHandler = () => {
+        this.setState({purchasing: true});
+    }
+
+    purchaseCancelHandler =() => {
+        this.setState({purchasing: false});
+    }
+
     render() {
         const disabledInfo = {
             ...this.state.ingredients
@@ -77,7 +87,7 @@ class BurgerBuilder extends Component {
         
         return(
             <Auxiliary>
-                <Modal>
+                <Modal show = {this.state.purchasing} modalClosed = {this.purchaseCancelHandler}>
                     <OrderSummary ingredients = {this.state.ingredients} />
                 </Modal>
                 <Burger ingredients={this.state.ingredients}/>
@@ -87,6 +97,7 @@ class BurgerBuilder extends Component {
                     disabled = {disabledInfo}
                     purchasable = {this.state.purchasable}
                     price = {this.state.totalPrice}
+                    ordered = {this.purchaseHandler}
                 />
             </Auxiliary>
         );
