e72a82f8146260b6f64a75a1e74af5941587f4d4
Kiran Kumbhar <kiran.kumbhar@protegrity.com>
2019-03-13 11:47:21 +0530

    Added custom button to modal

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -72,8 +72,12 @@ class BurgerBuilder extends Component {
         this.setState({purchasing: true});
     }
 
-    purchaseCancelHandler =() => {
+    purchaseCancelHandler = () => {
         this.setState({purchasing: false});
+    }
+
+    purchaseContinueHandler = () => {
+        alert('You continue!');
     }
 
     render() {
@@ -88,7 +92,12 @@ class BurgerBuilder extends Component {
         return(
             <Auxiliary>
                 <Modal show = {this.state.purchasing} modalClosed = {this.purchaseCancelHandler}>
-                    <OrderSummary ingredients = {this.state.ingredients} />
+                    <OrderSummary 
+                        ingredients = {this.state.ingredients}
+                        price = {this.state.totalPrice}
+                        purchaseCancelled = {this.purchaseCancelHandler}
+                        purchaseContinued = {this.purchaseContinueHandler} 
+                    />
                 </Modal>
                 <Burger ingredients={this.state.ingredients}/>
                 <BuildControls
