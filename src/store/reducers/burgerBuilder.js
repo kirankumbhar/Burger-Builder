c2957c3a8ae93f97e6b19d99d8ae83f28d6d47ff
Kiran Kumbhar <kiran.kumbhar@protegrity.com>
2019-07-30 23:28:04 +0530

    Redux advanced Completed

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -15,25 +15,33 @@ const INGRIEDIENT_PRICES = {
 
 }
 
+const addIngredient = (state, action) => {
+    const updatedIngredient = { [action.ingredientName]: state.ingredients[action.ingredientName] + 1 }
+    const updatedIngredints = updateObject(state.ingredients, updatedIngredient);
+    const updatedState = {
+        ingredients: updatedIngredints,
+        totalPrice: state.totalPrice + INGRIEDIENT_PRICES[action.ingredientName]
+    }
+    return updateObject(state, updatedState)
+}
+
+const removeIngredient = (state, action) => {
+    const updatedIng = { [action.ingredientName]: state.ingredients[action.ingredientName] - 1 }
+    const updatedIngs = updateObject(state.ingredients, updatedIng);
+    const updatedSt = {
+        ingredients: updatedIngs,
+        totalPrice: state.totalPrice - INGRIEDIENT_PRICES[action.ingredientName]
+    }
+    return updateObject(state, updatedSt)
+}
+
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.ADD_INGREDIENT:
-            const updatedIngredient = { [action.ingredientName]: state.ingredients[action.ingredientName] + 1 }
-            const updatedIngredints = updateObject(state.ingredients, updatedIngredient);
-            const updatedState = {
-                ingredients: updatedIngredints,
-                totalPrice: state.totalPrice + INGRIEDIENT_PRICES[action.ingredientName]
-            }
-            return updateObject(state, updatedState)
+            return addIngredient(state, action)
 
         case actionTypes.REMOVE_INGREDIENT:
-            const updatedIng = { [action.ingredientName]: state.ingredients[action.ingredientName] - 1 }
-            const updatedIngs = updateObject(state.ingredients, updatedIng);
-            const updatedSt = {
-                ingredients: updatedIngs,
-                totalPrice: state.totalPrice - INGRIEDIENT_PRICES[action.ingredientName]
-            }
-            return updateObject(state, updatedSt)
+            return removeIngredient(state, action)
 
         case actionTypes.SET_INGREDIENT:
             return updateObject(state, {
